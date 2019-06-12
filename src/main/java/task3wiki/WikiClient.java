f9566feab98547ce20f8790bff19334ce65057b0
Nikita <nik.ignition.work@gmail.com>
2019-06-12 11:11:28 +0300

    added  remove last method and now history of articles in deque collection

--- src/main/java/task3wiki/WikiClient.java
+++ src/main/java/task3wiki/WikiClient.java
@@ -4,20 +4,25 @@ import java.util.Date;
 
 public class WikiClient {
 
-  public static void main(String[] args) throws CloneNotSupportedException{
+  public static void main(String[] args) throws CloneNotSupportedException {
     WikiPrototype javaArticle = new WikiPrototype("This is old  text for Java 1.5", new Date().getTime());
 
     WikiPrototype newJavaArticle = javaArticle.clone();
     newJavaArticle.changeArticleText("Java 12 yeah!");
 
-    WikiPrototype evenNewerJavaArticall = newJavaArticle.clone();
-    evenNewerJavaArticall.changeArticleText("Java 12 is under new licence!");
+    WikiPrototype evenNewerJavaArticle = newJavaArticle.clone();
+    evenNewerJavaArticle.changeArticleText("Java 12 is under new licence!");
 
     System.out.println("init article");
     javaArticle.printArticle();
     System.out.println("new article ");
     newJavaArticle.printArticle();
-    System.out.println("newwest article");
-    evenNewerJavaArticall.printArticle();
+    System.out.println("newer article");
+    evenNewerJavaArticle.printArticle();
+
+    System.out.println("after removing");
+    evenNewerJavaArticle.removeLast();
+    evenNewerJavaArticle.printArticle();
+
   }
 }
--- src/main/java/task3wiki/WikiPrototype.java
+++ src/main/java/task3wiki/WikiPrototype.java
@@ -1,30 +1,40 @@
 package task3wiki;
 
-import java.util.Date;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class WikiPrototype implements Cloneable {
-  private StringBuilder articleText;
+  private Deque<String> articleText;
 
-  public WikiPrototype(String inputText, long timestamp) {
-    articleText = new StringBuilder();
-    articleText.append(inputText)
-            .append(" with timestamp + " + timestamp);
+  WikiPrototype(String inputText, long timestamp) {
+    articleText = new ArrayDeque<>();
+    articleText.add(inputText + " with timestamp " + timestamp);
   }
 
-  private WikiPrototype(String savedArticleText) {
-    articleText = new StringBuilder(savedArticleText);
+  private WikiPrototype(Deque<String> savedArticleText) {
+    articleText = savedArticleText;
   }
 
-  public void changeArticleText(String newArticleText) {
-    articleText.append(" + " + newArticleText);
+  void changeArticleText(String newArticleText) {
+    articleText.add(" + " + newArticleText);
   }
 
-  public void printArticle() {
+  void printArticle() {
     System.out.println(articleText);
+  }
+
+  void removeLast() {
+    articleText.remove(articleText.pollLast());
+  }
+
+  private Deque<String> getArticleTextHistory() {
+    return articleText;
   }
 
   @Override
   public WikiPrototype clone() throws CloneNotSupportedException {
-    return new WikiPrototype(this.articleText.toString());
+    Deque<String> tmp = new ArrayDeque<>(this.getArticleTextHistory());
+
+    return new WikiPrototype(tmp);
   }
 }
